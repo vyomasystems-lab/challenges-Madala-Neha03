65fa145045223a8cb2eba848ea239280d6063cec
Madala-Neha03 <99884583+Madala-Neha03@users.noreply.github.com>
2022-07-24 09:41:06 +0530

    Update test_mux.py

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -5,6 +5,26 @@ from cocotb.triggers import Timer
 
 @cocotb.test()
 async def test_mux(dut):
-    """Test for mux2"""
+    """Test for mux12"""
 
-    cocotb.log.info('##### CTB: Develop your test here ########')
+    x=12
+    A = 1
+   
+    dut.inp12.value = A
+    dut.sel.value = x
+    
+    await Timer(2, units='ns')
+    assert dut.out.value == L, f"Mux result is incorrect: {dut.X.value}!=1"
+
+
+    @cocotb.test()
+async def test_ran_mux(dut):
+    """Test for mux30"""
+
+     y=30
+     B = 2
+
+     dut.inp30.value = B
+     dut.sel.value = y
+    await Timer(2, units='ns')
+    assert dut.out.value == e, f"Mux result is incorrect: {dut.X.value}!=2"
