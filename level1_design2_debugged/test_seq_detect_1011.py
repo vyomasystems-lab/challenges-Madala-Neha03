f4a03a4bf7107b1804e8a7143f724662dfbf6447
Madala-Neha03 <99884583+Madala-Neha03@users.noreply.github.com>
2022-07-30 12:13:35 +0530

    Create test_seq_detect_1011.py

---
+++ level1_design2_debugged/test_seq_detect_1011.py
@@ -0,0 +1,39 @@
+# See LICENSE.vyoma for details
+
+# SPDX-License-Identifier: CC0-1.0
+
+import os
+import random
+from pathlib import Path
+
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, FallingEdge
+
+@cocotb.test()
+async def test_seq_bug1(dut):
+    """Test for seq detection """
+
+    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    cocotb.start_soon(clock.start())        # Start the clock
+
+    # reset
+    dut.reset.value = 1
+    await FallingEdge(dut.clk)  
+    dut.reset.value = 0
+    await FallingEdge(dut.clk)
+
+    cocotb.log.info('#### CTB: Develop your test here! ######')
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut._log.info(f"curr_state={dut.current_state.value}")
+    dut._log.info(f"next_state={dut.next_state.value}")
+    assert dut.current_state.value == dut.SEQ_1.value,f"Output is incorrect {dut.current_state}!={dut.SEQ_1.value}"
+    
+    
+
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut._log.info(f"curr_state={dut.current_state.value}")
+    dut._log.info(f"next_state={dut.next_state.value}")
+    assert dut.current_state.value == dut.SEQ_1.value,f"Output is incorrect {dut.current_state}!={dut.SEQ_1.value}"
