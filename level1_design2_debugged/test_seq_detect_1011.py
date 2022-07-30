35f8baa58337f217b0bf7044041a930c342e077e
Madala-Neha03 <99884583+Madala-Neha03@users.noreply.github.com>
2022-07-30 14:33:20 +0530

    Update test_seq_detect_1011.py

--- level1_design2_debugged/test_seq_detect_1011.py
+++ level1_design2_debugged/test_seq_detect_1011.py
@@ -24,16 +24,48 @@ async def test_seq_bug1(dut):
     await FallingEdge(dut.clk)
 
     cocotb.log.info('#### CTB: Develop your test here! ######')
-    dut.inp_bit.value = 1
-    await FallingEdge(dut.clk)
-    dut._log.info(f"curr_state={dut.current_state.value}")
-    dut._log.info(f"next_state={dut.next_state.value}")
-    assert dut.current_state.value == dut.SEQ_1.value,f"Output is incorrect {dut.current_state}!={dut.SEQ_1.value}"
-    
-    
 
     dut.inp_bit.value = 1
     await FallingEdge(dut.clk)
     dut._log.info(f"curr_state={dut.current_state.value}")
     dut._log.info(f"next_state={dut.next_state.value}")
-    assert dut.current_state.value == dut.SEQ_1.value,f"Output is incorrect {dut.current_state}!={dut.SEQ_1.value}"
+    assert dut.seq_seen == 0,f"Output is incorrect {dut.seq_seen}!=0"
+    assert dut.current_state.value==dut.SEQ_1.value,f"output is incorrect {dut.current_state!=dut.SEQ_1.value}"
+    
+
+    dut.inp_bit.value = 0
+    await FallingEdge(dut.clk)
+    dut._log.info(f"curr_state={dut.current_state.value}")
+    dut._log.info(f"next_state={dut.next_state.value}")
+    assert dut.seq_seen == 0,f"Output is incorrect {dut.seq_seen}!=0"
+    assert dut.current_state.value==dut.SEQ_10.value,f"output is incorrect {dut.current_state!=dut.SEQ_10.value}"
+
+
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut._log.info(f"curr_state={dut.current_state.value}")
+    dut._log.info(f"next_state={dut.next_state.value}")
+    assert dut.seq_seen == 0,f"Output is incorrect {dut.seq_seen}!=0"
+    assert dut.current_state.value==dut.SEQ_101.value,f"output is incorrect {dut.current_state!=dut.SEQ_101.value}"
+
+
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut._log.info(f"curr_state={dut.current_state.value}")
+    dut._log.info(f"next_state={dut.next_state.value}")
+    assert dut.seq_seen == 0,f"Output is incorrect {dut.seq_seen}!=0"
+    assert dut.current_state.value==dut.SEQ_1011.value,f"output is incorrect {dut.current_state!=dut.SEQ_1011.value}"
+
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut._log.info(f"curr_state={dut.current_state.value}")
+    dut._log.info(f"next_state={dut.next_state.value}")
+    assert dut.seq_seen == 1,f"Output is incorrect {dut.seq_seen}!=1"
+    assert dut.current_state.value==dut.SEQ_1.value,f"output is incorrect {dut.current_state!=dut.SEQ_1011.value}"
+
+
+
+    
+
+
+    
