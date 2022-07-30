ed090c5c948dd6ba97f23f60355f007a99cfbab3
Madala-Neha03 <99884583+Madala-Neha03@users.noreply.github.com>
2022-07-30 15:09:06 +0530

    Create shift_register.v

---
+++ level3_design/shift_register.v
@@ -0,0 +1,32 @@
+module shift_reg(op,clk,rst, load,sel,ip);
+  output reg [3:0] op;
+  input load;
+  input [1:0] sel;
+  input [3:0] ip;
+  input clk, rst;
+  
+  always @(posedge clk or posedge rst)
+   begin
+     if (rst)
+       
+       op = 0;
+      
+     else    
+       case(load)
+         1'b1: 
+          begin                                 //Load Input
+            op = ip;
+          end
+          
+         1'b0:                                 //Operation 
+           case (sel)
+            2'b00:  op = op>>1;                //Left Shift
+            2'b01:  op = op<<1;               //Right Shift
+            2'b10:  op = {op[2:0],op[3]};    //Rotate Left
+            2'b11:  op = {op[0], op[3:1]};  //Rotate Right
+          endcase                                     
+       endcase
+   
+   end 
+    
+  endmodule
